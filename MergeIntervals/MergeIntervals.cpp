987d4743ce62b39631ea64686ac210ea8a0abbf5
iamsidk721 <sidk721567@gmail.com>
2020-03-14 11:24:21 +0000

    [LocalTest]:[MergeIntervals]:[c++]

--- MergeIntervals/MergeIntervals.cpp
+++ MergeIntervals/MergeIntervals.cpp
@@ -9,9 +9,11 @@ class MergeIntervals {
  vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> result;
-        for(int i=1;i<=intervals.size();i++){
+        int x=intervals.size();
+		for(int i=1;i<=x;i++){
+			
             vector<int> insidearray;
-            if(i<intervals.size() && intervals[i-1][1] >= intervals[i][0]){
+            if(i<x && intervals[i-1][1] >= intervals[i][0]){
                 intervals[i][0] = min(intervals[i-1][0],intervals[i][0]);
                 intervals[i][1] = max(intervals[i-1][1], intervals[i][1]);
             }

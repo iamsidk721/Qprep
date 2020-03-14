d33fa827dface154238b49a66d61cc78e274b229
iamsidk721 <sidk721567@gmail.com>
2020-03-14 17:23:24 +0000

    [LocalTest]:[SpiralMatrix]:[c++]

--- SpiralMatrix/SpiralMatrix.cpp
+++ SpiralMatrix/SpiralMatrix.cpp
@@ -5,13 +5,55 @@
 using namespace std;
 
 class SpiralMatrix {
-public:
-    // Implement your solution by completing the below function	
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> res;
-
-        return res;
-    }
+ private:
+        int m; // rows
+        int n; // cols
+        vector<int> result;
+    
+    public:
+        vector<int> spiralOrder(vector<vector<int>>& matrix)
+        {
+            m = matrix.size();
+            if (m < 1)
+                return result;
+            n = matrix[0].size();
+            
+            int rowLoop = 0;
+            int colLoop = 1;
+            
+            int row = -1; // Ensure that it initially starts at 0
+            int col = -1; // Ensure that it initially starts at 0
+            
+            // Amount of horizontal/vertical traversals
+            while(result.size() < m*n)
+            {
+                // Left to Right
+                int limit = rowLoop++/2;
+                for (row++, col++; col < n - limit; col++)
+                    result.push_back(matrix[row][col]);
+                
+                // Top to Bottom
+                limit = colLoop++/2;
+                for (row++, col--; row < m - limit; row++)
+                    result.push_back(matrix[row][col]);
+                
+                // Half way exit
+                if (result.size() >= m*n)
+                    break;
+                
+                // Right to Left
+                limit = rowLoop++/2;
+                for (row--, col--; col >= 0 + limit; col--)
+                    result.push_back(matrix[row][col]);
+                
+                // Bottom to Top
+                limit = colLoop++/2;
+                for (row--, col++; row >= 0 + limit; row--)
+                    result.push_back(matrix[row][col]);
+            }
+            
+            return result;
+        }
 };
 
 int main() {

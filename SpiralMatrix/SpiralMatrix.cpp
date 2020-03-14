ed46e51de6fae8ff60503349fb566e29d88a4abf
iamsidk721 <sidk721567@gmail.com>
2020-03-14 17:30:15 +0000

    [LocalTest]:[SpiralMatrix]:[c++]

--- SpiralMatrix/SpiralMatrix.cpp
+++ SpiralMatrix/SpiralMatrix.cpp
@@ -5,55 +5,45 @@
 using namespace std;
 
 class SpiralMatrix {
- private:
-        int m; // rows
-        int n; // cols
-        vector<int> result;
+ public:
+	vector<int> spiralOrder(vector<vector<int>>& matrix) {
+		vector<int> result;
+		if (matrix.empty()) return result;
+
+    int r = matrix.size();
+    int c = matrix[0].size();
     
-    public:
-        vector<int> spiralOrder(vector<vector<int>>& matrix)
-        {   int x=result.size();
-            m = matrix.size();
-            if (m < 1)
-                return result;
-            n = matrix[0].size();
-            
-            int rowLoop = 0;
-            int colLoop = 1;
-            
-            int row = -1; // Ensure that it initially starts at 0
-            int col = -1; // Ensure that it initially starts at 0
-            
-            // Amount of horizontal/vertical traversals
-            while(x < m*n)
-            {
-                // Left to Right
-                int limit = rowLoop++/2;
-                for (row++, col++; col < n - limit; col++)
-                    result.push_back(matrix[row][col]);
-                
-                // Top to Bottom
-                limit = colLoop++/2;
-                for (row++, col--; row < m - limit; row++)
-                    result.push_back(matrix[row][col]);
-                
-                // Half way exit
-                if (x >= m*n)
-                    break;
-                
-                // Right to Left
-                limit = rowLoop++/2;
-                for (row--, col--; col >= 0 + limit; col--)
-                    result.push_back(matrix[row][col]);
-                
-                // Bottom to Top
-                limit = colLoop++/2;
-                for (row--, col++; row >= 0 + limit; row--)
-                    result.push_back(matrix[row][col]);
-            }
-            
-            return result;
+	//sr: start row, sc: start column, er: end row, ec: end column
+    int sr=0, sc=0, er=r-1, ec=c-1;
+    while(sr <=er && sc <=ec) {
+        if (sr == er) {
+            for (int i=sc; i<=ec; i++)
+                result.push_back(matrix[sr][i]);
+            break;
         }
+        if (sc==ec) {
+            for (int i=sr; i<=er; i++) {
+                result.push_back(matrix[i][sc]);
+            }
+            break;
+        }
+
+        for (int i=sc; i< ec; i++) {
+            result.push_back(matrix[sr][i]);
+        }
+        for (int i=sr; i < er; i++) {
+            result.push_back(matrix[i][ec]);
+        }
+        for (int i=ec; i>sc; i--) {
+            result.push_back(matrix[er][i]);
+        }
+        for (int i=er; i>sr; i--) {
+            result.push_back(matrix[i][sc]);
+        }
+        sr++;sc++;er--;ec--;
+    }
+    return result;
+}
 };
 
 int main() {

05e8f3e71767b229142661d72dfe5fb70a277591
iamsidk721 <sidk721567@gmail.com>
2020-03-14 07:53:31 +0000

    [LocalTest]:[LetterCombinationsOfPhoneNumber]:[c++]

--- LetterCombinationsOfPhoneNumber/LetterCombinationsOfPhoneNumber.cpp
+++ LetterCombinationsOfPhoneNumber/LetterCombinationsOfPhoneNumber.cpp
@@ -9,18 +9,22 @@ public:
     vector<string> letterCombinations(string digits) {
     	vector<string> res;
         string chmp[10]={"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-		for (int i = 0; i < digits.size(); i++)
+		int x=digits.size();
+		int z=res.size();
+
+		for (int i = 0; i < x; i++)
     {
         vector<string> tempres;
         string chs = chmp[digits[i] - '0'];
+		int y=chs.size();
         cout<<"chars = "<<chs<<endl;
         cout<<"digits[i] = "<<digits[i]<<endl;
         cout<<"digits[i] - '0' = "<<digits[i] - '0'<<endl;
 
 
-        for (int c = 0; c < chs.size();c++)
+        for (int c = 0; c < y;c++)
         {
-            for (int j = 0; j < res.size();j++)
+            for (int j = 0; j < z;j++)
             {
                 cout<<"res[j]= "<<res[j]<<endl;
                 cout<<"chars[c]= "<<chs[c]<<endl;
@@ -29,7 +33,7 @@ public:
 
                 tempres.push_back(res[j]+chs[c]);
             }
-            cout<<"==============="<<endl;
+            cout<<endl;
         }
         res = tempres;
     }

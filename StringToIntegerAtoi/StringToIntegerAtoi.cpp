eda1c303698c3795932621b10796098ae80fd8fb
iamsidk721 <sidk721567@gmail.com>
2020-03-15 07:29:26 +0000

    [LocalTest]:[StringToIntegerAtoi]:[c++]

--- StringToIntegerAtoi/StringToIntegerAtoi.cpp
+++ StringToIntegerAtoi/StringToIntegerAtoi.cpp
@@ -1,13 +1,19 @@
 #include <bits/stdc++.h>
 #include "../crio/cpp/io/FastIO.hpp"
+#include <sstream>
 using namespace std;
 
 class StringToIntegerAtoi {
 public:
     // Implement your solution by completing the below function	
     int myAtoi(string str) {
-    	int ans = 0;
-
+		int ans = stoi(str),f=0;
+        for(int i=0;i<str.size();i++)
+		{
+		if(str[i]=='-')
+		f=1;}
+		if(f==1)
+		ans=ans*(-1);
         return ans;
     }
 };

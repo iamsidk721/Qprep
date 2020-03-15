f56da3e62e654f1d37638608c80ee40126f86864
iamsidk721 <sidk721567@gmail.com>
2020-03-15 09:36:26 +0000

    [LocalTest]:[ValidParenthesis]:[c++]

--- ValidParenthesis/ValidParenthesis.cpp
+++ ValidParenthesis/ValidParenthesis.cpp
@@ -2,14 +2,79 @@
 #include "../crio/cpp/io/FastIO.hpp"
 using namespace std;
 
-class ValidParenthesis {
+class Solution {
 public:
-    // Implement your solution by completing the below function	
-    bool isValid(string s) {
-	return true;
+    bool isValid(string str) {
+        stack<char> s;
+	    bool ans=true;
+	    for(int i=0;i<str.length();i++)
+	    {
+	        if(str[i]=='{' or str[i]=='(' or str[i]=='[')
+	        {
+	            s.push(str[i]);
+	        }
+	        else
+	        {
+	            if(str[i]=='}')
+	            {
+	                if(!s.empty() and s.top()=='{')
+	                {
+	                    s.pop();
+	                }
+	                else if(!s.empty() and s.top()!='{')
+	                {
+	                    ans=false;
+	                    break;
+	                }
+	                else if(s.empty())
+	                {
+	                    ans=false;
+	                    break;
+	                }
+	            }
+	            else if(str[i]==')')
+	            {
+	                if(!s.empty() and s.top()=='(')
+	                {
+	                    s.pop();
+	                }
+	                else if(!s.empty() and s.top()!='(')
+	                {
+	                    ans=false;
+	                    break;
+	                }
+	                else if(s.empty())
+	                {
+	                    ans=false;
+	                    break;
+	                }
+	            }
+	            else if(str[i]==']')
+	            {
+	                if(!s.empty() and s.top()=='[')
+	                {
+	                    s.pop();
+	                }
+	                else if(!s.empty() and s.top()!='[')
+	                {
+	                    ans=false;
+	                    break;
+	                }
+	                else if(s.empty())
+	                {
+	                    ans=false;
+	                    break;
+	                }
+	            }
+	        }
+	    }
+	    if(!s.empty())
+	    {
+	        ans=false;
+	    }
+        return ans;
     }
 };
-
 int main() {
 	FastIO();
 	string s;
